fe86905db3a4381e0bf6e97c7dec43df5901fbc6
AlliGrass <alligrasson@gmail.com>
2025-07-06 17:15:29 +1000

    adjusted database

--- src/components/sections/NavBar.tsx
+++ src/components/sections/NavBar.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import ThemeSwitcher from "../ui/tempThemeSwitch";
 import { useNavigationRefs } from "@/context/NavigationRefsContext";
+import { useContentData } from "@/context/ContentDataContext";
+import parseData from "@/lib/db/parseData";
 
 
 export default function NavBar({currentPage, changeCurrentPage}: any) {
+    const { pageContent } = useContentData()
     const {projectRef, experienceRef} = useNavigationRefs()
 
     const sectionsDetails = {
@@ -15,10 +18,14 @@ export default function NavBar({currentPage, changeCurrentPage}: any) {
         if (sectionsDetails[navInput] !== currentPage) changeCurrentPage()
         sectionRef.current?.scrollIntoView({ behaviour: "smooth", block: "start"})
     }
+    
+    const logoDetails = parseData([pageContent, "personalLinks"]).find((socialDetails) => socialDetails.social === "logo")
+    console.log(logoDetails)
 
     return (
-        <div className="bg-[linear-gradient(0deg,#F0F0FF47_0%,#424874_11%,#f0f0ff_14%)] dark:bg-[linear-gradient(0deg,#1a1a1d47_0%,#A64D79_11%,#1a1a1d_14%)] sticky top-0 z-10 p-2 flex justify-between">
-            <div>Logo</div>
+        <div className="h-15  bg-[linear-gradient(0deg,#F0F0FF47_0%,#424874_11%,#f0f0ff_14%)] dark:bg-[linear-gradient(0deg,#1a1a1d47_0%,#A64D79_11%,#1a1a1d_14%)] sticky top-0 z-10 p-2 flex justify-between">
+            <a className="object-contain" href={logoDetails.href_link}><img className="h-auto" src={logoDetails.img_src} alt={logoDetails.social} /></a>
+            {/* <div>Logo</div> */}
             <div>
                 <button onClick={() => navigateSection("projects", projectRef)}>Project</button>
                 <button onClick={() => navigateSection("experience", experienceRef)}>Experience</button>
