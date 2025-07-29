5e37ac2e4c07c9284840afa89698dc22f96ee75a
Allison <alligrasson@gmail.com>
2025-07-29 17:54:03 +1000

    Button Correction

--- src/components/sections/base/Skills.tsx
+++ src/components/sections/base/Skills.tsx
@@ -40,7 +40,7 @@ export default function Skills({updateTags, selectedTags}: SkillProps) {
                                 return <img className="w-8 p-1 border rounded-full m-1 bg-[linear-gradient(30deg,#8086B0_0%,#f0f0ff_44%,#A6B1E1_79%)] border-defined-light dark:bg-[linear-gradient(30deg,#59244A_0%,#450C32_44%,#A64D79_79%)] dark:border-defined-dark" key={index} src={tagDetails.img_src} alt={tagDetails.title}/>
                             }) }
                         </div>
-                        <Button buttonTitle={"Clear Filter"} linkedRef={clearFilters} isFunction={true}/>
+                        <Button buttonTitle={"Clear Filter"} linkedRef={clearFilters}/>
                     </>
                 }
             </div>
